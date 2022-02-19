f9e41615ba872f25f10b929992ff3cfc0016a6b1
Ahmed Mohamed Hamed <ahmedhamed111111111@gmail.com>
2022-02-19 17:27:49 +0200

    User defined conversion

---
+++ User_Defines_Conversion_Operators/User_Defines_Conversion_Operators/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+using User_Defines_Conversion_Operators;
+
+Guid id = Guid.NewGuid();
+
+// implicit conversion
+UserId userId = id; 
+Guid guild = userId;
+
+
+// explicit conversion
+// implicit conversion can be used for explicit alse
+UserId userId1 = (UserId)id;
+Guid guild1 = (Guid)userId1;
---
+++ User_Defines_Conversion_Operators/User_Defines_Conversion_Operators/UserId.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Defines_Conversion_Operators
+{
+    public class UserId
+    {
+        public UserId(Guid value)
+        {
+            Value = value;
+        }
+
+        public Guid Value { get; }
+
+        // convert from guid to userid
+        public static implicit operator UserId(Guid guid)
+        {
+            return new UserId(guid);
+        }
+
+        // convert from userid to guid
+        public static implicit operator Guid(UserId userId)
+        {
+            return userId.Value;
+        }
+
+        // convert from guid to userid
+        //public static explicit operator UserId(Guid guid)
+        //{
+        //    return new UserId(guid);
+        //}
+
+        // convert from userid to guid
+        //public static explicit operator Guid(UserId userId)
+        //{
+        //    return userId.Value;
+        //}
+    }
+}

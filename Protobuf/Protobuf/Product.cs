2bec71651d2a982abca8178beb96faad5bf09143
Ahmed Mohamed Hamed <ahmedhamed111111111@gmail.com>
2022-02-20 10:46:21 +0200

    protobuf

---
+++ Protobuf/Protobuf/Product.cs
@@ -0,0 +1,30 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protobuf
+{
+    [ProtoContract]
+    public class Product
+    {
+        [ProtoMember(1)]
+        public int Id { get; set; }
+
+        [ProtoMember(2)]
+        public string? Name { get; set; }
+
+        [ProtoMember(3)]
+        public int Quantity { get; set; }
+
+        [ProtoMember(4)]
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Quantity: {Quantity}, Price: {Price}";
+        }
+    }
+}
---
+++ Protobuf/Protobuf/Program.cs
@@ -0,0 +1,47 @@
+﻿using Protobuf;
+using ProtoBuf;
+using System.Text;
+
+Product product1 = new() {
+    Id = 1,
+    Name = "tv",
+    Quantity = 10,
+    Price = 1000.0
+};
+
+Product product2 = new()
+{
+    Id = 2,
+    Name = "phone",
+    Quantity = 5,
+    Price = 1500.0
+};
+
+Product product3 = new()
+{
+    Id = 3,
+    Name = "pc",
+    Quantity = 20,
+    Price = 2000.0
+};
+
+var productsBeforeSerialize = new List<Product>() { product1, product2, product3 };
+
+using (var file = File.Create("D:\\person.bin"))
+{
+    Serializer.Serialize(file, productsBeforeSerialize);
+}
+
+List<Product> productsAfterDeSerialize;
+using (var file = File.OpenRead("D:\\person.bin"))
+{
+    productsAfterDeSerialize = Serializer.Deserialize<List<Product>>(file);
+}
+
+foreach (var product in productsAfterDeSerialize)
+    Console.WriteLine(product);
+
+Console.ReadKey();
+
+
+

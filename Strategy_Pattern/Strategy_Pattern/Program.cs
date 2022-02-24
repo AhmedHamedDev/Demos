542d4c304477239490b37cda71265c1b52afa255
Ahmed Mohamed Hamed <ahmedhamed111111111@gmail.com>
2022-02-24 19:46:58 +0200

    strategy pattern

---
+++ Strategy_Pattern/Strategy_Pattern/Program.cs
@@ -0,0 +1,10 @@
+﻿using Strategy_Pattern;
+
+Context context = new Context(new OperationAdd());
+Console.WriteLine("10 + 5 = " + context.executeStrategy(10, 5));
+
+context = new Context(new OperationSubstract());
+Console.WriteLine("10 - 5 = " + context.executeStrategy(10, 5));
+
+context = new Context(new OperationMultiply());
+Console.WriteLine("10 * 5 = " + context.executeStrategy(10, 5));
